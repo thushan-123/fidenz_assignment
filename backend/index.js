4e952d53ea3389298b58c4ed2034dadfac32e013
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-02 17:49:42 +0530

    run the app

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from "database/db.js"
+import connectDb from "database/db.js"
 import helmet from 'helmet';
 import 'dotenv/config'
 import cors from "cors"
@@ -11,3 +11,20 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+
+
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
+
+connectDb(MONGO_URI).then(
+    () => app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT} :) :)`);
+    }))
+    .catch(
+        (error) => {
+            console.log(error.message);
+            process.exit(1);
+        }
+    )
