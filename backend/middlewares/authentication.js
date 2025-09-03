896143ba93ad51cb9c1689001f721af6e2445bc1
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-03 23:15:54 +0530

    attach to user credentials

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -1,23 +1,37 @@
 import jwt from 'jsonwebtoken';
 
+
 // [ 'method': 'end point' ]
 // [ {'post' : 'api/v1/login'}, {} , {} ]
+
+const jwtSecret = process.env.JWT_SECRET;
 
 const permitAccessEndPoints = async (permitAccessList) => {
 
 }
 
 const authentication = async (req, res, next) => {
-    const header = req.headers.authorization || '';
+    try {
 
-    const token = header.startsWith('Bearer ') ? header.slice(7) : '';
+        const header = req.headers.authorization || '';
 
-    if (!token) {
-        if (permitAccessEndPoints){
-            next();
+        const token = header.startsWith('Bearer ') ? header.slice(7) : '';
+
+        if (!token) {
+            res.status(401).json({
+                message: 'unauthorized access',
+            })
         }
-        res.status(401).json({
-            message: 'unauthorized access',
-        })
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        if (payload) {
+            req.user = payload;
+            next();
+        }else {
+            res.status(401).json({
+                message: 'unauthorized access',
+            })
+        }
+    }catch(e) {
+        console.log(e.message)
     }
 }
