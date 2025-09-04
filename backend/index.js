89e59c0ed14cd9512821496b461d9fd17816415c
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-04 15:28:21 +0530

    add to prefix

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,11 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use("/api/v1", (req, res, next) => {
+    next();
+});
+
 app.use(authentication)
 
 app.use(user);
