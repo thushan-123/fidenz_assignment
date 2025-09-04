3576afebbeba7c469b47072a1336d614d96c5786
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-04 09:39:39 +0530

    gen jwt token

---
+++ backend/controllers/auth.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+const genAccessToken = async (payload) => {
+
+    return await jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRE,
+        })
+}
